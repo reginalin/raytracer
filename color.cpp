c32975873bc937888eb2bbd0ac9fae135bb71174
Ree <reginalin714@gmail.com>
2017-04-25 15:02:47 -0400

    started lambert

--- color.cpp
+++ color.cpp
@@ -6,11 +6,12 @@ Color::Color(unsigned char r, unsigned char g, unsigned char b)
     this->g = g;
     this->b = b;
 
-
 }
 
 void Color::lambert(Intersection intersection) {
-
+    glm::vec3 norm = intersection.normal;
+    //light direction is vector from light source to ...
+    glm::vec3 light;
 }
 
 
